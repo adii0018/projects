c9934f1bb98fe201eabc4f4b1b1f1079c7003ac4
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-04-26 23:32:14 +0530

    01

--- 01.py
+++ 01.py
@@ -1,1 +1,27 @@
-print("hey")
+import numpy as np
+from PIL import Image
+
+# image load karo
+img = Image.open("input.jpg")
+img_array = np.array(img)
+
+# --------- 1. GRAYSCALE ----------
+gray = np.mean(img_array, axis=2)
+gray_img = gray.astype(np.uint8)
+
+Image.fromarray(gray_img).save("gray.jpg")
+
+
+# --------- 2. BRIGHTNESS INCREASE ----------
+bright = img_array + 50
+bright = np.clip(bright, 0, 255)
+
+Image.fromarray(bright.astype(np.uint8)).save("bright.jpg")
+
+
+# --------- 3. NEGATIVE IMAGE ----------
+negative = 255 - img_array
+
+Image.fromarray(negative.astype(np.uint8)).save("negative.jpg")
+
+print("Done bhai 🚀")
